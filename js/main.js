a0e076825b52ca6ab199ed9994d6203da18dd634
Bai Yan <dingdingbai0424@gmail.com>
2016-09-29 12:31:16 +0800

    修改list栏样式，添加删除功能

--- js/main.js
+++ js/main.js
@@ -5,13 +5,16 @@ $("#addtodo").click(function(evt){
   if (task === ""){
     $("#inputTask").addClass('error')
   }else{
-    $("#listTask").append("<li>"+"<p>"+task+"</p>"
-                +"</li>"+"<hr>"+"</hr>");
+    $("#listTask").append("<li>"+"<input class='toggle' type='checkbox'></input>"+"<p>"+task+"</p>"
+                +"<span class='close'>x</span>"+"</li>");
 
     $('#inputTask').removeClass('error')
   }
  
  $("#inputTask").val("")
 
+ $('.close').click(function(){
+  $(this).closest('li').remove()
+ })
 })
 
