43fb5d424c8925ca4e592d1f8eb816b1f3f75a90
Bai Yan <dingdingbai0424@gmail.com>
2016-09-30 18:52:04 +0800

    修改filters点击样式

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var counter = 0
-
+//点击按钮添加todo事件
 $("#addtodo").click(function(evt){
   var task = $("#inputTask").val()
   if (task === "") {
     $("#inputTask").addClass('error')
   } else {
     counter++
-    var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+    var $li = $("<li><input class='toggle' name='items' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
     $("#listTask").append($li)
     $('#inputTask').removeClass('error')
     $('#todo-count').text(counter + "item left")
@@ -18,19 +18,18 @@ $("#addtodo").click(function(evt){
     })
     $("#inputTask").val("")
   }
-
 })
 
-
-
+//按enter键添加todo事件
 document.addEventListener("keydown", function(e){
   if (e.keyCode === 13) {
     var task = $("#inputTask").val()
     if (task === "") {
       $("#inputTask").addClass('error')
     } else {
+//事件数量统计     
       counter++
-      var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+      var $li = $("<li><input class='toggle' name='items' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
       $("#listTask").append($li)
       $('#inputTask').removeClass('error')
       $('#todo-count').text(counter + "item left")
@@ -45,15 +44,14 @@ document.addEventListener("keydown", function(e){
   }
 })
 
-
-
-
-
-
+//filter点击添加样式
 $('#filters li').click(function(){
-  $(this).css({
-    "border": "1px solid transparent",
-    "border-color":"rgba(175, 47, 47, 0.2)",
-    "cursor": "pointer"
-  })
+  $(this).addClass('selected').siblings().removeClass('selected')
 })
+
+
+// $('.toggle').click(function(){
+//   $('[name=items]:checkbox').addClass('checked')
+//     alert("123")
+
+// })
