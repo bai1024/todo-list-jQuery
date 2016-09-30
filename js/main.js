0c24756f74f9cb166adc58472d0897c2b35c4e5a
Bai Yan <dingdingbai0424@gmail.com>
2016-09-30 13:07:42 +0800

    增添计数新功能

--- js/main.js
+++ js/main.js
@@ -1,20 +1,59 @@
+var counter = 0
+
 $("#addtodo").click(function(evt){
-  evt.preventDefault()
   var task = $("#inputTask").val()
-
-  if (task === ""){
+  if (task === "") {
     $("#inputTask").addClass('error')
-  }else{
-    $("#listTask").append("<li>"+"<input class='toggle' type='checkbox'></input>"+"<p>"+task+"</p>"
-                +"<span class='close'>x</span>"+"</li>");
-
+  } else {
+    counter++
+    var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+    $("#listTask").append($li)
     $('#inputTask').removeClass('error')
-  }
- 
- $("#inputTask").val("")
+    $('#todo-count').text(counter + "item left")
 
- $('.close').click(function(){
-  $(this).closest('li').remove()
- })
+    $li.find(".close").click(function(){
+     $(this).closest('li').remove()
+     counter--
+      $('#todo-count').text(counter + "item left")
+    })
+    $("#inputTask").val("")
+  }
+
 })
 
+
+
+document.addEventListener("keydown", function(e){
+  if (e.keyCode === 13) {
+    var task = $("#inputTask").val()
+    if (task === "") {
+      $("#inputTask").addClass('error')
+    } else {
+      counter++
+      var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+      $("#listTask").append($li)
+      $('#inputTask').removeClass('error')
+      $('#todo-count').text(counter + "item left")
+
+      $li.find(".close").click(function(){
+       $(this).closest('li').remove()
+       counter--
+        $('#todo-count').text(counter + "item left")
+      })
+      $("#inputTask").val("")
+    }
+  }
+})
+
+
+
+
+
+
+$('#filters li').click(function(){
+  $(this).css({
+    "border": "1px solid transparent",
+    "border-color":"rgba(175, 47, 47, 0.2)",
+    "cursor": "pointer"
+  })
+})
