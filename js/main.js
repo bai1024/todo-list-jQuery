baf32dc83fdafdff50603ad888c93c8a22258ef9
Bai Yan <dingdingbai0424@gmail.com>
2016-10-09 10:09:36 +0800

    更新fliter功能

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $("#addtodo").click(function(evt){
   if (task === "") {
     $("#inputTask").addClass('error')
   } else {
-    counter++
-    var $li = $("<li><input class='toggle' name='items' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+    var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
     $("#listTask").append($li)
     $('#inputTask').removeClass('error')
+    counter++
     $('#todo-count').text(counter + "item left")
 
     $li.find(".close").click(function(){
@@ -16,8 +16,15 @@ $("#addtodo").click(function(evt){
      counter--
       $('#todo-count').text(counter + "item left")
     })
+
     $("#inputTask").val("")
-  }
+
+    $li.find('.toggle').click(function(){
+      $(this).closest('li').addClass('completed')
+    //   counter--
+    // $('#todo-count').text(counter + "item left")
+    })
+  } 
 })
 
 //按enter键添加todo事件
@@ -29,7 +36,7 @@ document.addEventListener("keydown", function(e){
     } else {
 //事件数量统计     
       counter++
-      var $li = $("<li><input class='toggle' name='items' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+      var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
       $("#listTask").append($li)
       $('#inputTask').removeClass('error')
       $('#todo-count').text(counter + "item left")
@@ -49,9 +56,26 @@ $('#filters li').click(function(){
   $(this).addClass('selected').siblings().removeClass('selected')
 })
 
+// 查看所有任务
+$('#all').click(function(){
+  $('.completed').siblings().show()
+  $('.completed').show()
+})
 
-// $('.toggle').click(function(){
-//   $('[name=items]:checkbox').addClass('checked')
-//     alert("123")
+//查看已完成任务
+$('#completed').click(function(){
+  $('.completed').siblings().hide()
+  $('.completed').show()
+})
 
-// })
+//查看未完成任务
+$('#active').click(function(){
+  $('.completed').hide()
+  $('.completed').siblings().show()
+})
+
+// 删除所有标记为完成的任务
+$('#clear-completed').click(function(){
+  $('.completed').remove()
+})
+
