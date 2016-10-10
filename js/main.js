e6db2bc67754f688b6d3850008dcd3071602a819
Bai Yan <dingdingbai0424@gmail.com>
2016-10-10 14:33:35 +0800

    功能全部实现

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $("#addtodo").click(function(evt){
   if (task === "") {
     $("#inputTask").addClass('error')
   } else {
-    var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+    var $li = $("<li class='active'><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
     $("#listTask").append($li)
     $('#inputTask').removeClass('error')
     counter++
@@ -19,10 +19,21 @@ $("#addtodo").click(function(evt){
 
     $("#inputTask").val("")
 
+//点击checkbox事件
     $li.find('.toggle').click(function(){
-      $(this).closest('li').addClass('completed')
-    //   counter--
-    // $('#todo-count').text(counter + "item left")
+      if ($(this).closest('li').hasClass('completed')){
+        $(this).closest('li').removeClass('completed')
+        $(this).closest('li').addClass('active')
+        counter++
+        $('#todo-count').text(counter + "item left")
+
+      } else {
+          $(this).closest('li').removeClass('active')
+          $(this).closest('li').addClass('completed')
+           counter--
+          $('#todo-count').text(counter + "item left")
+
+      }
     })
   } 
 })
@@ -36,7 +47,7 @@ document.addEventListener("keydown", function(e){
     } else {
 //事件数量统计     
       counter++
-      var $li = $("<li><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
+      var $li = $("<li class='active'><input class='toggle' type='checkbox'></input><p>"+ task +"</p><span class='close'>x</span></li>")
       $("#listTask").append($li)
       $('#inputTask').removeClass('error')
       $('#todo-count').text(counter + "item left")
@@ -46,7 +57,24 @@ document.addEventListener("keydown", function(e){
        counter--
         $('#todo-count').text(counter + "item left")
       })
+
       $("#inputTask").val("")
+
+      $li.find('.toggle').click(function(){
+        if ($(this).closest('li').hasClass('completed')){
+          $(this).closest('li').removeClass('completed')
+          $(this).closest('li').addClass('active')
+          counter++
+          $('#todo-count').text(counter + "item left")
+
+        } else {
+            $(this).closest('li').removeClass('active')
+            $(this).closest('li').addClass('completed')
+             counter--
+            $('#todo-count').text(counter + "item left")
+
+        }
+      })
     }
   }
 })
@@ -58,21 +86,22 @@ $('#filters li').click(function(){
 
 // 查看所有任务
 $('#all').click(function(){
-  $('.completed').siblings().show()
-  $('.completed').show()
-})
-
-//查看已完成任务
-$('#completed').click(function(){
-  $('.completed').siblings().hide()
+  $('.active').show()
   $('.completed').show()
 })
 
 //查看未完成任务
 $('#active').click(function(){
   $('.completed').hide()
-  $('.completed').siblings().show()
+  $('.active').show()
 })
+
+//查看已完成任务
+$('#completed').click(function(){
+  $('.active').hide()
+  $('.completed').show()
+})
+
 
 // 删除所有标记为完成的任务
 $('#clear-completed').click(function(){
